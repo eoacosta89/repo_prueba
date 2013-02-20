bbde43d0de7e7efc936acc1b8cf8967f85b35ec1
Emilson Acosta <emilson@ubuntu.(none)>
2013-02-20 05:25:44 -0600

    Modificacion de la clase Main, imprimir el valor de una variable

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,10 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int numero=0;
 		System.out.println("Proyecto de prueba");
 		System.out.println("Codigo inicial a subir a git");
+		System.out.println("EL valor es: "+numero); 
 	}
 
 }
